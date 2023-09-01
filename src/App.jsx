da35fefa489011a0195512445bf5b23b485efbf5
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-09-01 20:20:24 +0800

    Third Commit
    8:17 PM
    9/1/2023
    
    - Adding styles to the web
      * Images
      * Borders
      * Adding Class Names
      * Setting up the based style of the passenger count
      * Changing the Header Icon and its Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Hero from "./components/Hero"
+import icon from "./images/train-station.jpg"
 
 export default function App() {
     return (
         <>
-            <Hero />
+            <div className="main">
+                <div className="container">
+                    <div className="container--bg-image"></div>
+                    <img src={icon} alt="An Image of a Train Station" className="container--image" />
+                    <Hero />
+                </div>
+            </div>
         </>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,12 +21,16 @@ export default function Hero() {
 
     return (
         <>
-            <h1>Passenger Count</h1>
-            <h4>{count}</h4>
-            <button onClick={increment}>INCREMENT</button>
-            <button onClick={save}>SAVE</button>
-            <button onClick={reset}>RESET</button>
-            <p>Previous Count: {previousCounts.join(' - ')}</p>
+            <div className="hero">
+                <h1 className="hero--title">Passenger Count</h1>
+                <h3 className="hero--count">{count}</h3>
+                <div className="buttons">
+                    <button onClick={increment} className="hero--increment-btn">INCREMENT</button>
+                    <button onClick={save} className="hero--save-btn">SAVE</button>
+                    <button onClick={reset} className="hero--reset-btn">RESET</button>
+                </div>
+                <p>Previous Count: {previousCounts.join(' - ')}</p>
+            </div>
         </>
     )
 }
